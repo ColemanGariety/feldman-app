e464b1ceeb5ea9f411462f3f2b3e5a882f124685
Brian Hendrickson <brian@megapump.com>
2016-02-03 02:59:11 -0800

    trying the midi.js example

--- app/index.js
+++ app/index.js
@@ -2,4 +2,23 @@ const React = require('react')
 const ReactDOM = require('react-dom')
 const render = ReactDOM.render
 
+
+window.onload = function () {
+	MIDI.loadPlugin({
+		soundfontUrl: "./soundfont/",
+		instrument: "acoustic_grand_piano",
+		onprogress: function(state, progress) {
+			console.log(state, progress)
+		},
+		onsuccess: function() {
+			var delay = 0
+			var note = 50
+			var velocity = 127
+			MIDI.setVolume(0, 127)
+			MIDI.noteOn(0, note, velocity, delay)
+			MIDI.noteOff(0, note, delay + 0.75)
+		}
+	})
+}
+
 render(React.createElement('div', {}, 'this is just a <div> tag rendered in React. you can open command line with ALT+COMMAND+I!'), document.body.firstElementChild)
